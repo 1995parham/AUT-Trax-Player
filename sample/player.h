baea3c8dc2ab0592275c27fd9f79d8302cb403bf
Parham Alvani <parham.alvani@gmail.com>
2016-02-26 23:54:24 +0330

    Player header was added.

---
+++ sample/player.h
@@ -0,0 +1,18 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : player.h
+ *
+ * [] Creation Date : 26-02-2016
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+/*
+ * Copyright (c) 2016 Parham Alvani.
+*/
+
+struct move {
+	int row;
+	int col;
+};
